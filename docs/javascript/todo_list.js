04daaff694ce84edfbd5fcd1dd039b8d1c757d5e
jhun1020 <jhun1020.kh@gmail.com>
2022-11-29 13:33:42 +0900

    todolist

---
+++ docs/javascript/todo_list.js
@@ -0,0 +1,36 @@
+let input_btn = document.querySelector(".input_btn");
+let input_task = document.querySelector(".input_task");
+// add클릭하면 input값 받아서 새로운 li 추가
+input_btn.addEventListener("click", (event) => {
+  if (input_task.value == "") {
+    alert("Please enter a task");
+  } else {
+    new_task();
+  }
+});
+
+let query_task_list = document.querySelector(".task_list");
+// 새로운  task입력
+function new_task() {
+  let new_task = `<li class="flex_between">
+  <div>${input_task.value}</div><div><span class="material-symbols-outlined favorite" id="favorite_false">favorite</span>
+  <span><i class="material-icons delete">delete</i></span>
+  </div>
+  </li>`;
+  query_task_list.insertAdjacentHTML("beforeend", new_task);
+}
+
+query_task_list.addEventListener("click", (event) => {
+  if (event.target.innerHTML == "delete") {
+    event.target.parentElement.parentElement.parentElement.remove();
+  }
+
+  if (event.target.id == "favorite_false") {
+    event.target.id = "favorite_true";
+    return;
+  }
+  if (event.target.id == "favorite_true") {
+    event.target.id = "favorite_false";
+    return;
+  }
+});
